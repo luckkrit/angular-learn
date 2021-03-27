1ff2e1e714b82cb8071041fd6bf1507f856255be
Krit Chomaitong <luckkrit@hotmail.com>
2021-03-27 18:18:13 +0700

    create component Navbar, About, Education and Employment

--- ch23-27/Homework1/portfolio/src/app/portfolio/portfolio.module.ts
+++ ch23-27/Homework1/portfolio/src/app/portfolio/portfolio.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PortfolioComponent } from './portfolio.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { EmploymentComponent } from './employment/employment.component';
 
 
 
 @NgModule({
-  declarations: [PortfolioComponent],
+  declarations: [PortfolioComponent, NavbarComponent, AboutComponent, EducationComponent, EmploymentComponent],
   imports: [
     CommonModule
   ]
