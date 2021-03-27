2dcf46a85bafb535622add15d4bf259eb93322f6
Krit Chomaitong <luckkrit@hotmail.com>
2021-03-27 18:16:10 +0700

    create component Portfolio

--- ch23-27/Homework1/portfolio/src/app/portfolio/portfolio.module.ts
+++ ch23-27/Homework1/portfolio/src/app/portfolio/portfolio.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PortfolioComponent } from './portfolio.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PortfolioComponent],
   imports: [
     CommonModule
   ]
