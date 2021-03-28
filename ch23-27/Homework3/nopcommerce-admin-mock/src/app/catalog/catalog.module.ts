b8ef3cd4b78de727081c5e6e3ca73c5fca09e3d8
Krit Chomaitong <luckkrit@hotmail.com>
2021-03-28 20:25:27 +0700

    Add admin-service module and update catalog module add product list and product details

--- ch23-27/Homework3/nopcommerce-admin-mock/src/app/catalog/catalog.module.ts
+++ ch23-27/Homework3/nopcommerce-admin-mock/src/app/catalog/catalog.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductListComponent, ProductDetailsComponent],
   imports: [
     CommonModule
   ]
