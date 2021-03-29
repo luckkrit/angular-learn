0ca21bd5a3aab8c60c88689a889dc779c3cb6fad
Krit Chomaitong <luckkrit@hotmail.com>
2021-03-29 19:08:15 +0700

    Add font awesome to dashboard module

--- ch23-27/Homework3/nopcommerce-admin-mock/src/app/dashboard/dashboard.module.ts
+++ ch23-27/Homework3/nopcommerce-admin-mock/src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MaterialModule } from '../material/material.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, FontAwesomeModule],
 })
 export class DashboardModule {}
