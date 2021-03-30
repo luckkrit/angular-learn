7372c399e7d4a786b28269957326457b666481f8
Krit Chomaitong <luckkrit@hotmail.com>
2021-03-30 21:43:04 +0700

    Add exchange-rates module and exchange-form

---
+++ ch28-32/Homework2/src/app/exchange-rates/exchange-form/exchange-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exchange-form',
+  templateUrl: './exchange-form.component.html',
+  styleUrls: ['./exchange-form.component.css']
+})
+export class ExchangeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ch28-32/Homework2/src/app/exchange-rates/exchange-rates.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExchangeFormComponent } from './exchange-form/exchange-form.component';
+
+
+
+@NgModule({
+  declarations: [ExchangeFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExchangeRatesModule { }
