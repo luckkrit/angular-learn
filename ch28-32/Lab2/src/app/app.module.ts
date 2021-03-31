1737995ed3727b5c12df0e94c3f4152c7f67b86e
Krit Chomaitong <luckkrit@hotmail.com>
2021-03-31 18:01:53 +0700

    init lab2

--- ch28-32/Lab2/src/app/app.module.ts
+++ ch28-32/Lab2/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyFormComponent } from './my-form/my-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MyFormComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
