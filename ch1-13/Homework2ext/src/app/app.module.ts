fea03e56e7b2e8607035a2d2a5efccda95652ae3
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-01 11:21:44 +0700

    Finish Homework 2 - add delete button

---
+++ ch1-13/Homework2ext/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {TodolistComponent} from './todolist/todolist.component';
+import {TaskComponent} from './todolist/task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodolistComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
