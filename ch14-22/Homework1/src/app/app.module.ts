577c03f2554fab006458321d40df6dcfc34c4b06
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-01 12:23:46 +0700

    Finish Homework1

--- ch14-22/Homework1/src/app/app.module.ts
+++ ch14-22/Homework1/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TodolistComponent} from './todolist/todolist.component';
+import {TaskComponent} from './todolist/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
