c228bd5f8592816d1e681ebbd350c721bf9ada47
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-01 10:41:27 +0700

    Finish Homework1

--- ch1-13/Homework1/src/app/app.module.ts
+++ ch1-13/Homework1/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { TopBannerComponent } from './home/top-banner/top-banner.component';
+import { CategoryMenuComponent } from './home/category-menu/category-menu.component';
+import { SearchBarComponent } from './home/navbar/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    TopBannerComponent,
+    CategoryMenuComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
